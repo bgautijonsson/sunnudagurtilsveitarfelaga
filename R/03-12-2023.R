c888c4523709749a12368729040d607daea72b89
bgautijonsson <bgautijonsson@gmail.com>
2023-12-03 10:25:02 +0000

    Initial commit

---
+++ R/03-12-2023.R
@@ -0,0 +1,133 @@
+library(tidyverse)
+library(readxl)
+library(here)
+library(glue)
+library(patchwork)
+library(hagstofa)
+
+d <- read_csv(
+  here("data", "arbok_stodugildi.csv")
+) |> 
+  filter(
+    sveitarfelag %in% c(
+      "Reykjavíkurborg",
+      "Garðabær",
+      "Kópavogsbær",
+      "Hafnarfjarðarkaupstaður",
+      "Mosfellsbær",
+      "Seltjarnarnesbær"
+    )
+  ) |> 
+  rename(
+    "Samtals" = stodugildi_total,
+    "Skólar" = stodugildi_skola,
+    "Leikskólar" = stodugildi_leikskola,
+    "Annað (Ekki skólar eða leikskólar)" = stodugildi_annad
+  ) |> 
+  pivot_longer(c(-ar, -sveitarfelag)) |> 
+  mutate(
+    name = fct_relevel(
+      name,
+      "Leikskólar",
+      "Skólar",
+      "Annað (Ekki skólar eða leikskólar)"
+    )
+  )
+
+mannfjoldi <- hg_data(
+  url = "https://px.hagstofa.is:443/pxis/api/v1/is/Ibuar/mannfjoldi/2_byggdir/sveitarfelog/MAN02005.px"
+) |> 
+  filter(
+    Kyn == "Alls",
+    Aldur == "Alls",
+    Sveitarfélag != "Alls"
+  ) |> 
+  collect() |> 
+  janitor::clean_names() |> 
+  rename(mannfjoldi = mannfjoldi_eftir_sveitarfelagi_kyni_og_aldri_1_januar_1998_2023) |> 
+  mutate(ar = parse_number(ar)) |> 
+  select(-kyn, -aldur)
+
+lims <- c(0.8, 1.75)
+
+d1 <- d |> 
+  mutate(
+    type = "Hrein fjölgun"
+  ) |> 
+  mutate(
+    value = value / value[ar == min(ar)],
+    .by = c(sveitarfelag, name)
+  ) 
+
+
+d2 <- d |> 
+  mutate(
+    type = "Fjölgun á hvern íbúa"
+  ) |> 
+  inner_join(
+    mannfjoldi,
+    by = join_by(sveitarfelag, ar)
+  ) |> 
+  mutate(
+    value = value / mannfjoldi,
+    value = value / value[ar == min(ar)],
+    .by = c(sveitarfelag, name)
+  )  
+
+
+p <- d1 |> 
+  bind_rows(
+    d2
+  ) |>
+  mutate(
+    type = fct_relevel(type, "Hrein fjölgun")
+  ) |> 
+  ggplot(aes(ar, value, col = sveitarfelag)) +
+  geom_hline(
+    yintercept = 1,
+    lty = 2,
+    alpha = 0.5,
+    linewidth = 0.5
+  ) +
+  geom_line(
+    linewidth = 1,
+    arrow = arrow(length = unit(0.25, "cm"), type = "closed")
+  ) +
+  scale_y_continuous(
+    labels = function(x) hlutf(x - 1),
+    breaks = breaks_pretty(5)
+  ) +
+  scale_colour_brewer(
+    palette = "Set1",
+    guide = guide_legend(nrow = 1, label.position = "top")
+  ) +
+  facet_grid(
+    cols = vars(name),
+    rows = vars(type)
+  ) +
+  coord_cartesian(clip = "off") +
+  theme(
+    legend.position = "top"
+  ) +
+  labs(
+    title = "Stöðugildum hefur fjölgað hraðar en íbúum hjá flestum sveitarfélögum á Höfuðborgarsvæðinu",
+    subtitle = "(%) fjölgun stöðugilda (hrein / á hvern íbúa) miðað við 2018",
+    x = NULL,
+    y = NULL,
+    col = NULL,
+    caption = "Reiknað út frá Árbókum Sveitarfélaga hjá www.samband.is"
+  )
+
+
+ggsave(
+  plot = p,
+  filename = here(
+    "Figures", "03-12-2023.png"
+  ),
+  width = 8, height = 0.5 * 8, scale = 1.6
+)
+
+
+
+
+p
---
+++ R/prep_stodugildi_data.R
@@ -0,0 +1,113 @@
+library(tidyverse)
+library(readxl)
+library(here)
+library(glue)
+
+read_stodugildi_total <- function(year) {
+  path <- here("data-raw", "arbok", "rbok-{year}-toflur.xlsx") |> glue()
+  read_xlsx(path, sheet = "Tafla 17", skip = 4) |> 
+    slice(-1) |> 
+    rename("svfn" = 1, "sveitarfelag" = 2, stodugildi_total = "stöðugildi") |> 
+    select(sveitarfelag, stodugildi_total) |> 
+    mutate(ar = year)
+}
+
+read_stodugildi_skola <- function(year) {
+  path <- here("data-raw", "arbok", "rbok-{year}-toflur.xlsx") |> glue()
+  read_excel(
+    path, 
+    sheet = "Tafla 18", 
+    range = "A7:N356"
+  ) |> 
+    select(
+      sveitarfelag = 3,
+      stodugildi_skola = 14
+    ) |> 
+    filter(
+      str_detect(sveitarfelag, "[Ss]amtals")
+    ) |> 
+    mutate(
+      sveitarfelag = str_replace(sveitarfelag, " [Ss]amtals", "") |> 
+        str_replace("^[0-9]+ ", "")
+    ) |> 
+    mutate(
+      ar = year
+    )
+}
+
+read_stodugildi_leikskola <- function(year) {
+  path <- here("data-raw", "arbok", "rbok-{year}-toflur.xlsx") |> glue()
+  read_excel(
+    path, 
+    sheet = "Tafla 19", 
+    range = "A8:Q406"
+  ) |> 
+    select(
+      sveitarfelag = 3,
+      stodugildi_leikskola = 17
+    ) |> 
+    filter(
+      str_detect(sveitarfelag, "samtals")
+    ) |> 
+    mutate(
+      sveitarfelag = str_replace(sveitarfelag, " samtals", "") |> 
+        str_replace("^[0-9]+ ", "")
+    ) |> 
+    mutate(
+      ar = year
+    )
+}
+
+d_leikskolar <- map_dfr(2018:2023, read_stodugildi_leikskola) |> 
+  mutate(
+    sveitarfelag = case_when(
+      str_detect(sveitarfelag, "Reykjavíkurborg") ~ "Reykjavíkurborg",
+      str_detect(sveitarfelag, "Hafnarfj") ~ "Hafnarfjarðarkaupstaður",
+      str_detect(sveitarfelag, "Seltjar") ~ "Seltjarnarnesbær",
+      TRUE ~ sveitarfelag
+    )
+  )
+
+
+d_total <- map_dfr(2018:2023, read_stodugildi_total) |> 
+  mutate(
+    sveitarfelag = case_when(
+      str_detect(sveitarfelag, "Reykjavíkurborg") ~ "Reykjavíkurborg",
+      str_detect(sveitarfelag, "Hafnarfj") ~ "Hafnarfjarðarkaupstaður",
+      TRUE ~ sveitarfelag
+    )
+  )
+
+
+
+d_skolar <- map_dfr(2018:2023, read_stodugildi_skola) |> 
+  mutate(
+    sveitarfelag = case_when(
+      str_detect(sveitarfelag, "Reykjavíkurborg") ~ "Reykjavíkurborg",
+      str_detect(sveitarfelag, "Hafnarfj") ~ "Hafnarfjarðarkaupstaður",
+      str_detect(sveitarfelag, "Seltjar") ~ "Seltjarnarnesbær",
+      TRUE ~ sveitarfelag
+    )
+  )
+
+
+d_total |> 
+  inner_join(
+    d_skolar,
+    by = join_by(sveitarfelag, ar)
+  ) |> 
+  inner_join(
+    d_leikskolar,
+    by = join_by(sveitarfelag, ar)
+  ) |> 
+  mutate(
+    stodugildi_annad = stodugildi_total - stodugildi_skola - stodugildi_leikskola,
+    sveitarfelag = case_when(
+      str_detect(sveitarfelag, "Reykjavíkurb") ~ "Reykjavíkurborg",
+      str_detect(sveitarfelag, "Hafnarfj") ~ "Hafnarfjarðarkaupstaður",
+      TRUE ~ sveitarfelag
+    )
+  ) |> 
+  write_csv(
+    here("data", "arbok_stodugildi.csv")
+  )
