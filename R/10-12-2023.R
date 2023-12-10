a06b4d4a487fa648de7bfb0e020f2a862ea3fa68
bgautijonsson <bgautijonsson@gmail.com>
2023-12-10 17:51:22 +0000

    Uppfæra mymdn

--- R/10-12-2023.R
+++ R/10-12-2023.R
@@ -37,7 +37,7 @@ mannfjoldi <- read_csv(here("data", "mannfjoldi_svf.csv")) |>
 
 
 
-p <- d |> 
+p1 <- d |> 
   mutate(
     Skólar = stodugildi_skola_kenn / stodugildi_skola,
     Leikskólar = stodugildi_leiksk_kenn / stodugildi_leikskola
@@ -67,33 +67,117 @@ p <- d |>
     limits = c(2018, 2023.5)
   ) +
   scale_y_continuous(
-    labels = label_hlutf(accuracy = 1)
+    labels = label_hlutf(accuracy = 1),
+    expand = expansion()
   ) +
   guides(
-    x = "axis_truncated",
+    x = guide_axis_truncated(trunc_lower = 2018, trunc_upper = 2022),
     y = "axis_truncated"
   ) +
+  coord_cartesian(clip = "off") +
   facet_grid(cols = vars(sveitarfelag), rows = vars(name), scales = "free_y") +
   labs(
     x = NULL,
-    y = "% með kennaramenntun",
-    title = "Almennt er lægri hlutfall starfsfólks með kennararéttindi en fyrir 5 árum",
+    y = NULL,
     subtitle = str_c(
-      "Hlutfall starfsfólks með kennara- eða leikskólakennaramenntun í sveitarfélögum Höfuðborgarsvæðis (2018 - 2023)"
+      "% starfsfólks með kennara- eða leikskólakennaramenntun"
     )
   ) +
   theme(
-    panel.spacing.x = unit(0.05, "snpc"),
+    # panel.spacing.x = unit(0.08, "snpc"),
     strip.text = element_text(size = 11),
-    axis.text.x = element_text(size = 8)
+    axis.text.x = element_blank(),
+    axis.line.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    plot.margin = margin(t = 5, r = 5, b = 2, l = 5),
+    plot.subtitle = element_text(face = "bold")
   )
+
+
+
+
+
+
+p2 <- d |> 
+  mutate(
+    Skólar = stodugildi_skola_kenn,
+    Leikskólar = stodugildi_leiksk_kenn,
+  ) |> 
+  select(sveitarfelag, ar, Skólar, Leikskólar) |> 
+  pivot_longer(c(-ar, -sveitarfelag)) |> 
+  inner_join(
+    mannfjoldi,
+    by = join_by(sveitarfelag, name, ar)
+  ) |> 
+  mutate(
+    barn_per_kennari = mannfjoldi / value
+  ) |> 
+  ggplot(aes(ar, barn_per_kennari)) +
+  stat_smooth(
+    data = ~rename(.x, svf = sveitarfelag),
+    geom = "line",
+    span = 0.9,
+    se = 0,
+    aes(group = svf),
+    alpha = 0.1,
+    linewidth = 0.3
+  ) +
+  stat_smooth(
+    geom = "line",
+    span = 0.9,
+    se = 0,
+    linewidth = 1.5,
+    arrow = arrow(length = unit(0.25, "cm"), type = "closed")
+  ) +
+  scale_x_continuous(
+    breaks = c(2018:2022),
+    expand = expansion(),
+    limits = c(2018, 2023.5)
+  ) +
+  scale_y_continuous(
+    expand = expansion()
+  ) +
+  guides(
+    x = guide_axis_truncated(trunc_lower = 2018, trunc_upper = 2022),
+    y = "axis_truncated"
+  ) +
+  coord_cartesian(clip = "off") +
+  facet_grid(cols = vars(sveitarfelag), rows = vars(name), scales = "free_y") +
+  labs(
+    x = NULL,
+    y = NULL,
+    title = NULL,
+    subtitle = str_c(
+      "Fjöldi barna á hvern kennaramenntaðan starfsmann"
+    )
+  ) +
+  theme(
+    # panel.spacing.x = unit(0.08, "snpc"),
+    strip.text.x = element_blank(),
+    axis.text.x = element_text(size = 8),
+    strip.background.x = element_blank(),
+    plot.margin = margin(t = 0, r = 5, b = 5, l = 5),
+    plot.subtitle = element_text(face = "bold")
+  )
+  
+
+p <- p1 + 
+  p2 + 
+  plot_layout(ncol = 1) +
+  plot_annotation(
+    title = "Hefur kennaramenntuðu starfsfólki fjölgað í takt við vonir?",
+    subtitle = str_c(
+      "Almennt er lægri % stöðugilda með kennararéttindi árið 2023 en 2018 og börnum á hvern kennaramenntaðan hefur helst fækkað í Hafnarfirði"
+    )
+  )
+
+p
 
 ggsave(
   plot = p,
   filename = here(
     "Figures", "10-12-2023.png"
   ),
-  width = 8, height = 0.4 * 8, scale = 1.6
+  width = 8, height = 0.621 * 8, scale = 1.6
 )
-
 
